1eac0d15cca26f5eb92fb3d65cd8a153260e2050
Nilson Chapagain <nilson.chapagain@gmail.com>
2022-11-15 11:46:02 -0600

    Roxygen export and description for optimalHbi

--- R/OptimalHbi.R
+++ R/OptimalHbi.R
@@ -1,3 +1,14 @@
+#'  Variable selection using shrinkage priors
+#'
+#'optimalHbi function will take b.i and H.b.i as input which comes from the result of TwoMeans function. It will return H: the optimal value of the tuning parameter.
+#' @param bi The values of the tuning parameter
+#' @param Hbi The estimated number of signals corresponding to each b.i
+#'
+#' @return the optimal value of tuning parameter and the associated H value
+#' @export
+#'
+#' @examples
+#' optimalHbi(bi, Hbi)
 optimalHbi <- function(bi, Hbi){
   bi = 0
   H = 0
