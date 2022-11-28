8b624b1784fe4c9a0b1b1896eaf68021f9c156cd
nilson01 <nilson.chapagain@gmail.com>
2022-11-28 02:15:58 -0600

    S2MVarSelection method implementation completed with 2 versions

--- R/S2MVarSelection.R
+++ R/S2MVarSelection.R
@@ -1,16 +1,70 @@
+
+                  ###########################
+                  ###########################
+           ########   S2MVarSelection    #########
+                  ###########################
+                  ###########################
+
 #'  Variable selection using shrinkage priors:: Sequential2Means
 #'
 #' S2MVarSelection function will take S2M: a list obtained from the 2Means.variables function and H: the estimated number of signals obtained from the optimal.b.i function. This will give out the important subset of variables for the Gaussian Linear model.
-#' @param S2M List obtained from the 2Means.variables function
+#'
+#' @param Beta matrix consisting of N posterior samples of p variables that is known either to user or from Sequential2Means function
 #' @param H Estimated number of signals obtained from the optimal.b.i function
 #'
-#' @return Important subset of variables for the Gaussian Linear model
+#' @return Indices of important subset of variables
 #' @export
 #'
 #' @examples
-S2MVarSelection <- function(S2M, H) {
-  p <- S2M$p
-  abs.post.median <- S2M$abs.post.median
+#'
+S2MVarSelection <- function(Beta, H) {
+
+  # number of covariates
+  p <- ncol(Beta)
+
+  # the medians of the absolute values of the posterior samples of each Beta vector
+  abs.post.median = seq(0, length = p)
+  for(i in 1:p){
+    abs.post.median[i]=median(abs(Beta[,i]))
+  }
+
+  # the indices of selected variables
   impVariablesGLM <- order(abs.post.median)[p:(p - H + 1)]
+
+  return(impVariablesGLM)
+}
+
+
+
+
+                    ##########################
+                    ##########################
+                    ########   V1    #########
+                    ##########################
+                    ##########################
+
+
+#'  Variable selection using shrinkage priors:: Sequential2Means
+#'
+#' S2MVarSelectionV1 function will take S2M: a list obtained from the 2Means.variables function and H: the estimated number of signals obtained from the optimal.b.i function. This will give out the important subset of variables for the Gaussian Linear model.
+#'
+#' @param S2M List obtained from the 2Means.variables function
+#' @param H Estimated number of signals obtained from the optimal.b.i function
+#'
+#' @return Indices of important subset of variables for the Gaussian Linear model
+#' @export
+#'
+#' @examples
+S2MVarSelectionV1 <- function(S2M, H) {
+
+  # number of covariates
+  p <- ncol(S2M$p)
+
+  # the medians of the absolute values of the posterior samples of each Beta vector
+  abs.post.median = S2M$abs.post.median
+
+  # the indices of selected variables
+  impVariablesGLM <- order(abs.post.median)[p:(p - H + 1)]
+
   return(impVariablesGLM)
 }
